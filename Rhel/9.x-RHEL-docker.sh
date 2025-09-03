519d6665ae3ba2ab7a847e2b35259a4a57b2cbf6
Ugur Bozkurt <94750332+ugurbzkrt@users.noreply.github.com>
2025-09-03 13:31:02 +0300

    Update 9.x-RHEL-docker.sh
    Remove docker-compose install step

--- Rhel/9.x-RHEL-docker.sh
+++ Rhel/9.x-RHEL-docker.sh
@@ -21,5 +21,5 @@ sudo systemctl enable --now docker
 
 sudo usermod -aG docker ${USER}
 
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.32.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+#sudo curl -L "https://github.com/docker/compose/releases/download/v2.32.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#sudo chmod +x /usr/local/bin/docker-compose
